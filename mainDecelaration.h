42d8709b488bd1fb385f45443b3930851b48a6ee
Jeffie <banaemohamed@gmail.com>
2024-12-25 13:38:50 +0100

    Small Changes and Syntax fixes

--- mainDecelaration.h
+++ mainDecelaration.h
@@ -1,32 +1,25 @@
 #pragma once
 
-#include <stdio.h>
-#include <stdlib.h>
-#include<string.h>
-#include<stdbool.h>
-#include"queue.h"
-
-
 #define MAX_SIZE 50
 
-typedef struct PersonalInformation{
+typedef struct PersonalInformation {
     char firstName[MAX_SIZE];
-    char lastName[MAX_SIZE]
+    char lastName[MAX_SIZE];
 }PersonalInformation;
 
-typedef struct Doctor{
-    PersonalInformation IP;//IP hiya mokhtasar ta3 PersonalInformation
+typedef struct Doctor {
+    PersonalInformation IP; //IP hiya mokhtasar ta3 PersonalInformation
     char specialization[MAX_SIZE];
-    char block//block li Responsable 3lih
+    char block //block li Responsable 3lih
 }Doctor;
 
-typedef struct Patient{
+typedef struct Patient {
     PersonalInformation IP;
     int age;
     char bloodType[3];
     bool emergencySituation;
-    char illnessOrInjury[MAX_SIZE];//مرض او اصابة
+    char illnessOrInjury[MAX_SIZE]; //مرض او اصابة
     char block;
-    int priority;//ndiroha 0 ida emergency
+    int priority; //ndiroha 0 ida emergency
     Doctor personalDoctor;
 }Patient;
--- poject_Algo.c
+++ poject_Algo.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include"queue.h"
 #include<stdbool.h>
+#include"queue.h"
 
 #define MAX_SIZE 50
 
