aaae7a2e677a638aaeb3a140ee00bf75a346f560
Kadri <thefirstname1234567@gmail.com>
2024-12-31 18:57:03 +0100

    the functions of the project for now

---
+++ functions.c
@@ -0,0 +1,13 @@
+#include "mainDeclaration.h"
+#include "queue.h"
+#include<stdbool.h>
+PatQueue AddPatient(Patient P , PatQueue *SQ , PatQueue *EQ){ //SQ for standard queue and EQ for emergency queue
+    if(P.emergencySituation== false){
+        Push(&SQ , P);
+        return SQ;
+    }
+    else{
+        Push(&EQ , P);
+        return EQ;
+    }
+}
