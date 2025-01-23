bc1ba8e595879683b8e306106314dafb5437f31f
Kadri <thefirstname1234567@gmail.com>
2025-01-23 11:39:18 +0100

    added some functions to keep AddPatient simpler

--- functions.h
+++ functions.h
@@ -1,6 +1,7 @@
 #pragma once
 
 // Put Every Function Declaration You Add here in the same order they are in functions.c
-
+int max(int a,int b);
+void HandlePatientId(Patient P,PatQueue *SQ,PatQueue *EQ);
 void AddPatient(Patient P, PatQueue *SQ, PatQueue *EQ, History* history);
 bool SearchPAt(PatQueue q, Patient p);
