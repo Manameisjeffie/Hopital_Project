7a550e1ba7618fc4a8424d42ae2d94342acaa8f3
The Handsome Monkeee King <62846952+Manameisjeffie@users.noreply.github.com>
2025-01-25 18:24:15 +0100

    Fixing Johnny's spaces for the 169th time

--- functions.h
+++ functions.h
@@ -1,8 +1,8 @@
 #pragma once
 
 // Put Every Function Declaration You Add here in the same order they are in functions.c
-int max(int a,int b);
-void HandlePatientId(Patient P,PatQueue *SQ,PatQueue *EQ);
+int max(int a, int b);
+void HandlePatientId(Patient P, PatQueue *SQ, PatQueue *EQ);
 void AddPatient(Patient P, PatQueue *SQ, PatQueue *EQ, History* history);
 bool SearchPAt(PatQueue sq, PatQueue eq, Patient p);
-void DeletePatient(Patient P,PatQueue *SQ,PatQueue *EQ);
+void DeletePatient(Patient P, PatQueue *SQ, PatQueue *EQ);
