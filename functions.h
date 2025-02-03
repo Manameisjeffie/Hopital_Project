45f05535b6fd83567b0cf6dfcd0879191cf8b89f
Jeffie <banaemohamed@gmail.com>
2025-02-04 00:27:52 +0100

    Changed the DeletePatient function signature to match what's in functions.c

--- functions.h
+++ functions.h
@@ -5,4 +5,4 @@ int max(int a, int b);
 void HandlePatientId(Patient P, PatQueue *SQ, PatQueue *EQ);
 void AddPatient(Patient P, PatQueue *SQ, PatQueue *EQ, History* history);
 bool SearchPAt(PatQueue sq, PatQueue eq, Patient *p);
-void DeletePatient(Patient P, PatQueue *SQ, PatQueue *EQ);
+void DeletePatient(PatQueue *SQ, PatQueue *EQ, History* history);
